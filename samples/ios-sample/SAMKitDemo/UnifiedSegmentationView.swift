ee2552c95e223b308e0b9a8cc4cb368eba675f1e
john-rocky <rockyshikoku@gmail.com>
2026-03-30 12:14:40 +0900

    Shorten long press duration from 0.5s to 0.3s

--- samples/ios-sample/SAMKitDemo/UnifiedSegmentationView.swift
+++ samples/ios-sample/SAMKitDemo/UnifiedSegmentationView.swift
@@ -76,7 +76,7 @@ struct UnifiedSegmentationView: View {
                                     if gestureStartTime == nil {
                                         gestureStartTime = Date()
                                         // Schedule long press check via timer
-                                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
                                             guard gestureStartTime != nil,
                                                   !isLifted,
                                                   hasVisibleMasks,
